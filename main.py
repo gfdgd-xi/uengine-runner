2da3e3962b52cd17e3ed39e375a6ba054c3cc870
gfdgd xi <3025613752@qq.com>
2021-07-07 15:44:22 +0800

    1.2.2 test

--- main.py
+++ main.py
@@ -30,9 +30,7 @@ import PIL.ImageTk as ImageTk
 
 def UninstallProgram(package):
     global fineUninstallApkHistory
-    #setting = {True: "", False: "-k"}
     Return = GetCommandReturn("pkexec /usr/bin/uengine-session-launch-helper -- uengine uninstall --pkg='{}'".format(package))
-    #Return = GetCommandReturn("adb shell pm uninstall {} {}".format(setting[cleanData], package))
     if os.path.exists("{}/.local/share/applications/{}.desktop".format(get_home(), package)):
         os.remove("{}/.local/share/applications/{}.desktop".format(get_home(), package))
     if os.path.exists("{}/{}.desktop".format(get_desktop_path(), package)):
@@ -82,13 +80,6 @@ def Button3Install():
 
 def InstallApk(path, quit = False):
     global findApkHistory
-    '''if not AdbRun():
-        if quit:
-            return
-        messagebox.showinfo(title="提示", message="你没有使用 adb 连接任何设备")
-        DisabledAndEnbled(False)
-        return'''
-    #commandReturn = GetCommandReturn("adb install '{}'".format(path))
     commandReturn = GetCommandReturn("pkexec /usr/bin/uengine-session-launch-helper -- uengine install --apk='{}'".format(path))
     iconSavePath = "{}/.local/share/icons/hicolor/256x256/apps/{}.desktop".format(get_home(), GetApkPackageName(path))
     SaveApkIcon(path, iconSavePath)
@@ -109,15 +100,10 @@ def DisabledAndEnbled(choose):
     userChoose = {True: tk.DISABLED, False: tk.NORMAL}
     a = userChoose[choose]
     combobox1.configure(state=a)
-    #combobox2.configure(state=a)
     combobox3.configure(state=a)
-    #checkButton1.configure(state=a)
-    #button1.configure(state=a)
     button2.configure(state=a)
     button3.configure(state=a)
-    #button4.configure(state=a)
     button5.configure(state=a)
-    #button6.configure(state=a)
     button7.configure(state=a)
     button8.configure(state=a)
 
@@ -324,7 +310,8 @@ def GetApkIconInApk(apkFilePath):
         if "application:" in line:
             line = line[line.index("icon='"): -1]
             line = line.replace("icon='", "")
-            line = line.replace("'", "")
+            if "'" in line:
+                line = line[0: line.index("'")]
             return line
 
 def SaveApkIcon(apkFilePath, iconSavePath):
@@ -351,12 +338,11 @@ def get_desktop_path():
 # 获取用户主目录
 def get_home():
     return os.path.expanduser('~')
-
 ###########################
 # 程序信息
 ###########################
 programUrl = "https://gitee.com/gfdgd-xi/uengine-runner"
-version = "1.2.1"
+version = "1.2.2(test)"
 goodRunSystem = "Linux"
 about = '''一个基于 Python3 的 tkinter 制作的 uengine APK 安装器
 版本：{}
@@ -428,20 +414,14 @@ frame1 = ttk.Frame(window)
 frame2 = ttk.Frame(window)
 frame3 = ttk.Frame(window)
 label1 = ttk.Label(window, text="要安装的 apk 路径：")
-#label2 = ttk.Label(window, text="要连接的设备的 IP（默认 IP 为 192.168.250.2）：")
 label3 = ttk.Label(window, text="要卸载的包名或程序对应的 APK 文件：")
 combobox1 = ttk.Combobox(window, width=100)
-#combobox2 = ttk.Combobox(window, width=100)
 combobox3 = ttk.Combobox(window, width=100)
-#button1 = ttk.Button(frame1, text="连接设备", command=ConnectPhoneIp)
 button2 = ttk.Button(window, text="浏览", command=FindApk)
 button3 = ttk.Button(frame2, text="安装", command=Button3Install)
-#button4 = ttk.Button(frame1, text="关闭 adb 软件进程", command=KillAdbProgress)
 button5 = ttk.Button(frame2, text="打开 uengine 应用列表", command=Button5Click)
-#button6 = ttk.Button(frame1, text="连接默认 IP", command=ConnectPhoneIpDefult)
 button7 = ttk.Button(window, text="浏览", command=ButtonClick7)
 button8 = ttk.Button(frame3, text="卸载", command=ButtonClick8)
-#checkButton1 = ttk.Checkbutton(frame3, text="保留软件数据", variable=checkButtonBool1)
 menu = tk.Menu(window, background="white")  # 设置菜单栏
 programmenu = tk.Menu(menu, tearoff=0, background="white")  # 设置“程序”菜单栏
 #adb = tk.Menu(menu, tearoff=0, background="white")
@@ -454,8 +434,6 @@ menu.add_cascade(label="帮助", menu=help)
 programmenu.add_command(label="清空软件历史记录", command=CleanProgramHistory)
 programmenu.add_separator()  # 设置分界线
 programmenu.add_command(label="退出程序", command=window.quit)  # 设置“退出程序”项
-#adb.add_command(label="adb 连接的设备", command=ShowAdbConnect)
-#adb.add_command(label="adb 连接的设备的所有软件包", command=ShowAdbInstallPackage)
 uengine.add_command(label="发送 uengine 应用列表到桌面", command=SendUengineAndroidListForDesktop)
 uengine.add_command(label="发送 uengine 应用列表到启动器", command=SendUengineAndroidListForLauncher)
 help.add_command(label="程序官网", command=OpenProgramURL)  # 设置“程序官网”项
@@ -463,31 +441,23 @@ help.add_separator()
 help.add_command(label="小提示", command=helps)  # 设置“小提示”项
 help.add_command(label="更新内容", command=UpdateThings)  # 设置“更新内容”项
 help.add_command(label="这个程序使用的程序列表（部分）", command=ShowUseProgram)  # 设置“更新内容”项
-#help.add_command(label="关于 adb", command=AboutAdb)  # 设置“关于这个程序”项
 help.add_command(label="关于这个程序", command=about_this_program)  # 设置“关于这个程序”项
 menu.configure(activebackground="white")
 help.configure(activebackground="white")
 uengine.configure(activebackground="white")
-#adb.configure(activebackground="white")
 programmenu.configure(activebackground="white")
 # 设置控件
 combobox3['value'] = fineUninstallApkHistory
-#combobox2['value'] = phoneIp
 combobox1['value'] = findApkHistory
 #
 win.config(menu=menu)  # 显示菜单栏
 label1.grid(row=2, column=0)
-#label2.grid(row=0, column=0)
 label3.grid(row=4, column=0)
 combobox1.grid(row=2, column=1)
-#combobox2.grid(row=0, column=1)
 combobox3.grid(row=4, column=1)
-#button1.grid(column=0, row=0)
 button2.grid(row=2, column=2)
 button3.grid(row=0, column=0)
-#button4.grid(column=1, row=0)
 button5.grid(row=0, column=1)
-#button6.grid(row=0, column=3)
 button7.grid(row=4, column=2)
 button8.grid(row=0, column=1)
 #checkButton1.grid(row=0, column=0)
@@ -496,73 +466,3 @@ frame2.grid(row=3, columnspa=3)
 frame3.grid(row=5, columnspa=3)
 window.pack()
 win.mainloop()
-
-#########################################
-# 废弃的代码
-#########################################
-'''def KillAdbProgress():
-    DisabledAndEnbled(True)
-    Return = GetCommandReturn("killall adb")
-    if Return is "":
-        Return = "进程已经杀死！"
-    messagebox.showinfo(title="tips", message=Return)
-    DisabledAndEnbled(False)'''
-
-'''def Button1Click():
-    if combobox2.get() is "":
-        messagebox.showerror(title="提示", message="信息没有填写完整，无法继续连接 IP")
-        return
-    DisabledAndEnbled(True)
-    threading.Thread(target=ConnectPhoneIp).start()'''
-
-'''def ConnectPhoneIp():
-    global phoneIp
-    messagebox.showinfo(title="提示", message=GetCommandReturn("adb connect '{}'".format(combobox2.get())))
-    phoneIp.append(combobox2.get())
-    combobox2['value'] = phoneIp
-    write_txt(get_home() + "/.config/uengine-runner/PhoneIp.json", str(json.dumps(ListToDictionary(phoneIp))))  # 将历史记录的数组转换为字典并写入
-    DisabledAndEnbled(False)'''
-
-'''def ConnectPhoneIpDefult(quit = False):
-    global phoneIp
-    Return = GetCommandReturn("adb connect '192.168.250.2'")
-    if quit:
-        print(Return)
-        return
-    messagebox.showinfo(title="提示", message=Return)
-    phoneIp.append("192.168.250.2")
-    combobox2['value'] = phoneIp
-    write_txt(get_home() + "/.config/uengine-runner/PhoneIp.json", str(json.dumps(ListToDictionary(phoneIp))))  # 将历史记录的数组转换为字典并写入
-    DisabledAndEnbled(False)'''
-
-'''def AdbRun():
-    Return = GetCommandReturn("adb devices").replace("\n", "").replace("List of devices attached", "").replace("* daemon not running; starting now at tcp:5037", "").replace("* daemon started successfully", "")
-    if Return is "":
-        return False
-    return True'''
-
-'''def AdbConnect():
-    return GetCommandReturn("adb devices")'''
-
-'''def ShowAdbConnect():
-    messagebox.showinfo(title="提示", message=AdbConnect())'''
-
-'''def AboutAdb():
-    messagebox.showinfo(message=GetCommandReturn("adb version"), title="关于 adb")'''
-
-'''def GetAllPackageName():
-    return GetCommandReturn("adb shell pm list packages")'''
-
-'''def ShowAdbInstallPackage():
-    mess = tk.Toplevel()
-    message = ttk.Frame(mess)
-    mess.resizable(0, 0)
-    mess.title("所有软件包")
-    textbox1 = tk.Text(message, width=100)
-    button1 = ttk.Button(message, text="确定", command=mess.withdraw)
-    textbox1.insert("0.0", GetAllPackageName())
-    textbox1.configure(state=tk.DISABLED)
-    textbox1.pack()
-    button1.pack(side="bottom")
-    message.pack()
-    mess.mainloop()'''

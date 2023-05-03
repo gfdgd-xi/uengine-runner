543571d66b52a2cc4271ce41b94bf0c1d79f5463
gfdgd_xi <3025613752@qq.com>
2023-05-03 09:00:06 +0800

    优化uengine-loading-binder

--- LoadingBinder/load.sh
+++ LoadingBinder/load.sh
@@ -3,6 +3,10 @@ if [[ `whoami` != root ]]; then
     echo 这不是 root 用户，结束
     exit 1
 fi
+if [ -d /dev/binderfs ]; then   
+    echo 已启动 binder，end
+    exit
+fi
 modprobe binder_linux
 mkdir /dev/binderfs
 mount -t binder binder /dev/binderfs

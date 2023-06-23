86bc122c5fd35443f367ce15b1d53fc8321888ce
gfdgd_xi <3025613752@qq.com>
2023-06-23 16:51:13 +0800

    修复在DEEPIN/UOS程序菜单检查到UENGINE不存在但是无法安装的问题

--- uengine-runner-applist-launch.sh
+++ uengine-runner-applist-launch.sh
@@ -14,16 +14,16 @@ else
 	if [ "$versionLower" = "deepin" ] || [ "$versionLower" = "uos" ]; then
 		echo "此系统为 Deepin/UOS，使用 apt 安装"
 		zenity --question --text="您还未安装 UEngine，是否现在安装？" --no-wrap
-		if [[ $? = 0 ]]; then
-			"$dir/uengine-runner-applist-launch.sh" deepin-terminal -C "pkexec apt install uengine -y"
+		if [[ $? == 0 ]]; then
+			"$dir/launch.sh" deepin-terminal -C "pkexec apt install uengine -y"
 		fi
 	else
 		# 因为安装器出现问题，所以废弃
 		zenity --error --text="请安装 UEngine 后继续" --no-wrap
 		echo "非 Deepin/UOS 系统，使用 shenmo 提供的脚本安装\n暂未保证此安装脚本一定安装成功"
 		zenity --question --text="您还未安装 UEngine，是否现在安装？" --no-wrap
-		if [[ $? = 0 ]]; then
-			deepin-terminal -C "bash /opt/apps/com.gitee.uengine.runner.spark/files/uengine-installer"
+		if [[ $? == 0 ]]; then
+			"$dir/launch.sh" deepin-terminal -C "bash /opt/apps/com.gitee.uengine.runner.spark/files/uengine-installer"
 		fi
 	fi
 fi
